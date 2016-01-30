c0b795d030c08f987dffd9438c267c6aee93c720
Lewis Cawthorne <llc@acm.org>
2016-01-30 12:35:01 -0500

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+# Users
 User.create!(name:  "Lewis Cawthorne",
              email: "LLC@acm.org",
              password:              "foobar",
@@ -34,8 +35,17 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(7)
 50.times do 
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
